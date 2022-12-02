dabcbe0875c143236bc9e2a90444f4a35b851df2
Shafaq Saud <shafaq.saud@gmail.com>
2022-12-02 00:10:16 -0500

    screenshot

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,32 +4,7 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-// // Fake data taken from initial-tweets.json
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd"
-//     },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ];
+
 $(document).ready(function() {
 
   $('#post-tweet').submit(function(event) {
@@ -102,6 +77,7 @@ $(document).ready(function() {
     });
 
   };
+  //function to load tweets
   const loadTweets = function() {
     $.ajax({
       type: "GET",
